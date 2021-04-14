2ffa70b216430c5563d0ea82e57b99d45127391d
Linus De Meyere <linus.demeyere@kuleuven.be>
2021-04-14 14:02:12 +0200

    Add index page when exporting with

---
+++ bids-validator-web/pages/index.js
@@ -0,0 +1,4 @@
+// A placeholder component to trigger an actual index.html file when using `next export`.
+// This component won't be used since _app.js renders its own component, without any children.
+
+export default () => null;
